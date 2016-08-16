0ba08d218618648aa132827c4b08ea48cc010338
unknown <Sanjay.Singh@ATMECSINLT-011.ATMECS.CORP>
2016-08-16 23:48:28 +0530

    Redux 1st commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'JavaScript Quickly' },
+        { title: 'Freedom at Midnight' },
+        { title: 'Teach yourself C++' },
+        { title: 'Head First Patterns' }
+    ]
+}
