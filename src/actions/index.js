fca4e4ad218e1debd329d270c95c6edcd2c4579b
unknown <Sanjay.Singh@ATMECSINLT-011.ATMECS.CORP>
2016-08-19 00:58:33 +0530

    Completed 48 Conditional Rendering.

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ export function selectBook(book) {
     // selectBook is an ActionCreator, need to return
     // an action with object having type & payload properties
     return {
-        kind: 'BOOK_SELECTED',
+        type: 'BOOK_SELECTED',
         payload: book
     }
 }
